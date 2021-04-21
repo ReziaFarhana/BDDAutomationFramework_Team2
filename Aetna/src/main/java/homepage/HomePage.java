375403cc98695bbf5b335940133e4bb7f7f1cfd7
arafat147 <arafat.rahman93@gmail.com>
2021-04-21 16:37:51 -0400

    BDD FrameWork

--- Aetna/src/main/java/homepage/HomePage.java
+++ Aetna/src/main/java/homepage/HomePage.java
@@ -1,9 +1,17 @@
 package homepage;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class HomePage {
 
 // Action Method class
 
     // Find By Annotation:
+    @FindBy (css = "input#gh-btn") public WebElement searchField;
+
+    public void writeonsearchField(){
+        searchField.sendKeys("Java Books");
+    }
 
 }
